f974e97aba77639234ca93fc3ad4c1e443a3f377
OrGadamo <brber223@gmail.com>
2022-07-20 18:24:05 +0300

    ts-class

--- main.ts
+++ main.ts
@@ -113,3 +113,328 @@ function getProductDetailsString(
   }, ${discount ? ` ${discount} Sale` : ``}`;
 }
 // console.log(getProductDetailsString("shirt","red","50%",30,32,38,40,42))
+//34
+class Car {
+  company: string;
+  model: string;
+  color: string;
+  constructor(companyName: string, carModel: string, carColor: string) {
+    this.company = companyName;
+    this.model = carModel;
+    this.color = carColor;
+  }
+}
+//35
+class Elephant {
+  height: number;
+  species: string;
+  age: number;
+  orgin: string;
+  constructor(
+    elaphantHeight: number,
+    elephantType: string,
+    age: number,
+    orgin: string
+  ) {
+    this.height = elaphantHeight;
+    this.species = elephantType;
+    this.age = age;
+    this.orgin = orgin;
+  }
+  getDetails() {
+    return `Type:${this.species} Orgin:${this.orgin} Age:${this.age} Height:${this.height}`;
+  }
+}
+//36
+class Pants {
+  size: number;
+  color: string;
+  fabric: string;
+  inStock: boolean;
+  constructor(
+    pantsSize: number,
+    pantsColor: string,
+    pantsFabric: string,
+    available: boolean
+  ) {
+    this.size = pantsSize;
+    this.color = pantsColor;
+    this.fabric = pantsFabric;
+    this.inStock = available;
+  }
+  private details(): string {
+    return `Size:${this.size} Color:${this.color} Fabric:${this.fabric} In Stock:${this.inStock}`;
+  }
+  get getDetails() {
+    return this.details();
+  }
+}
+// let zara = new Pants(32,"black","cotton",true);
+// console.log(zara.getDetails);
+//37
+class Athlete {
+  fullName: string;
+  dateOfBirth: string;
+  field: string;
+  gender?: string;
+  constructor(
+    fullName: string,
+    dateOfBirth: string,
+    field: string,
+    gender?: string
+  ) {
+    this.fullName = fullName;
+    this.dateOfBirth = dateOfBirth;
+    this.field = field;
+    this.gender = gender;
+  }
+  protected details(): string {
+    return `Full Name:${this.fullName} Date Of Birth:${
+      this.dateOfBirth
+    } Field:${this.field} ${this.gender ? `Gender:${this.gender}` : ""}`;
+  }
+  get getDetails() {
+    return this.details();
+  }
+}
+// let or = new Athlete("or gadamo","1.2.332","soccer","male");
+// console.log(or.getDetails)
+//38
+class FootballPlayer extends Athlete {
+  inNationalTeam: boolean;
+  team: string;
+  constructor(
+    fullName: string,
+    dateOfBirth: string,
+    field: string,
+    inNationalTeam: boolean,
+    team: string,
+    gender?: string
+  ) {
+    super(fullName, dateOfBirth, field, gender);
+    this.team = team;
+    this.inNationalTeam = inNationalTeam;
+  }
+  getPlayerDetails(): string {
+    return `${super.getDetails} Team:${this.team} In National Team:${
+      this.inNationalTeam
+    }`;
+  }
+}
+//39
+class User {
+  fullName: string;
+  age: number;
+  password: string;
+  private _connected: boolean = false;
+  constructor(fullName: string, age: number, password: string) {
+    this.age = age;
+    this.fullName = fullName;
+    this.password = password;
+  }
+  get getConnected(): boolean {
+    return this._connected;
+  }
+  set setConnected(bool: boolean) {
+    this._connected = bool;
+  }
+  static getUserByAge(sortBY: string, ...users: User[]) {
+    if (users) {
+      switch (sortBY) {
+        case "old":
+          return users.sort((a: User, b: User) => b.age - a.age)[0];
+        case "young":
+          return users.sort((a: User, b: User) => a.age - b.age)[0];
+      }
+    }
+    return;
+  }
+}
+// let ori = new User("or gadamo",23,"or1111")
+// let ori2 = new User("or gadamo",21,"or1111")
+// console.log(User.getUserByAge("old",ori,ori2))
+//40
+class Student extends User {
+  fName: string;
+  lNAME: string;
+  dateOfBirth: string;
+  email: string;
+  constructor(
+    fName: string,
+    lName: string,
+    dateOfBirth: string,
+    email: string,
+    age: number,
+    password: string
+  ) {
+    super(`${fName} ${lName}`, age, password);
+    this.fName = fName;
+    this.lNAME = lName;
+    this.dateOfBirth = dateOfBirth;
+    this.email = email;
+  }
+  getIsConnected(): string {
+    return this.getConnected ? "green" : "red";
+  }
+}
+// let foo = new Student("or","gadamo","1.2.31","email",23,"yyref12");
+let lior = new Student("lior", "David", "1.1.1", "safdasd", 26, "asfdasd");
+lior.setConnected = false;
+//41
+class Room {
+  width: number;
+  height: number;
+  area: number;
+  private isWindows: boolean = false;
+  constructor(width: number, height: number) {
+    this.height = height;
+    this.width = width;
+    this.area = height * width;
+  }
+  getDetails(): string {
+    return `Width:${this.width} Height:${this.height} Area:${this.area} Are There Windows:${this.isWindow}`;
+  }
+  get getIsWindows(): boolean {
+    return this.isWindows;
+  }
+  set setIsWindows(bool: boolean) {
+    this.isWindows = bool;
+  }
+  static getRoomByArea(sortBy: string, ...rooms: Room[]) {
+    if (rooms) {
+      switch (sortBy) {
+        case "min":
+          return rooms.sort((a, b) => a.area - b.area)[0];
+        case "max":
+          return rooms.sort((a, b) => b.area - a.area)[0];
+      }
+    }
+    return;
+  }
+}
+//42
+class Kitchen extends Room {
+  closetsNum: number;
+  constructor(width: number, height: number, closetsNum: number) {
+    super(width, height);
+    this.closetsNum = closetsNum;
+  }
+  getKitchenDetails(): string {
+    return `${this.getDetails()} Closets Number:${this.closetsNum}`;
+  }
+  getMessageByIsWindow(): string {
+    return this.getIsWindows ? "have Windows" : "close Room";
+  }
+}
+//43
+class LivingRoom extends Room {
+  seatsNum: number;
+  airDirection: string;
+  constructor(
+    width: number,
+    height: number,
+    seatsNum: number,
+    airDirection: string
+  ) {
+    super(width, height);
+    this.seatsNum = seatsNum;
+    this.airDirection = airDirection;
+  }
+  printLivingRoomDetails() {
+    console.log(
+      `${super.getDetails} Number Of Seats:${this.seatsNum} Air Direction:${
+        this.airDirection
+      }`
+    );
+  }
+  getMessageByIsWindow(): string {
+    return this.getIsWindows
+      ? `have Windows Air Direction:${this.airDirection}`
+      : `close Room`;
+  }
+}
+//44
+class Product {
+  width: number;
+  height: number;
+  weight: number;
+  price: number;
+  private isOrdered: boolean = false;
+  constructor(width: number, height: number, weight: number, price: number) {
+    this.width = width;
+    this.height = height;
+    this.weight = weight;
+    this.price = price;
+  }
+  get getIsOrdered(): boolean {
+    return this.isOrdered;
+  }
+  set setIsOrdered(bool: boolean) {
+    this.isOrdered = bool;
+  }
+  static getProductByPrice(sortBy = "high", ...products: Product[]) {
+    if (products) {
+      switch (sortBy) {
+        case "low":
+          return products.sort(
+            (a: Product, b: Product) => a.price - b.price
+          )[0];
+        case "high":
+          return products.sort(
+            (a: Product, b: Product) => b.price - a.price
+          )[0];
+      }
+    }
+    return;
+  }
+  getProductDetails(): string {
+    return `width:${this.width} height:${this.height} weight:${this.weight} price:${this.price}`;
+  }
+}
+//45
+class Computer extends Product {
+  output: number;
+  constructor(
+    width: number,
+    height: number,
+    weight: number,
+    price: number,
+    output: number
+  ) {
+    super(width, height, weight, price);
+    this.output = output;
+  }
+  getComputerDetails(): string {
+    return `${this.getProductDetails()} output:${this.output}`;
+  }
+  getMessageByIsOrdered() {
+    return this.getIsOrdered ? "ordered" : "need to order";
+  }
+}
+//46
+class ComputerScreen extends Product {
+  orderNum: number;
+  resulotion: string;
+  constructor(
+    width: number,
+    height: number,
+    weight: number,
+    price: number,
+    orderNum: number,
+    resulotion: string
+  ) {
+    super(width, height, weight, price);
+    this.orderNum = orderNum;
+    this.resulotion = resulotion;
+  }
+  getComputerScreenDetails(): string {
+    return `${this.getProductDetails()} resulotion:${
+      this.resulotion
+    } number of orders:${this.orderNum}`;
+  }
+  getMessageByIsOrdered() {
+    return this.getIsOrdered
+      ? `${this.orderNum} have been ordered`
+      : "need to order";
+  }
+}
